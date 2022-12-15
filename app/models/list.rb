24e7b77b4470098076524209af07421d2ef3e392
kouji2004 <mrtaxi48@gmail.com>
2022-12-15 10:17:18 +0000

    投稿機能にバリデーション機能を追加

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,10 @@
 class List < ApplicationRecord
   has_one_attached :image
+
+# presence（存在）をチェックします。
+# trueと記述すると、データが存在しなければならないという設定になります
+
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :image, presence: true
 end
